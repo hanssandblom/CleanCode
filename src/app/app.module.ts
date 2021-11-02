95cedfc9a3caed6a3ce0dee5ad9b421e26d8b9bc
Hans Sandblom <hans.sandblom@iths.se>
2021-11-02 17:17:32 +0100

    added Game of Life component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GolComponent } from './gol/gol/gol.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GolComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/gol/gol/gol.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gol',
+  templateUrl: './gol.component.html',
+  styleUrls: ['./gol.component.css']
+})
+export class GolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
