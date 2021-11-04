bba5506e80de4214df5240b75ab5b9fc9e23f8bb
Hans Sandblom <hans.sandblom@iths.se>
2021-11-04 14:34:04 +0100

    corrected html for app and added test again for launching app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,4 +25,11 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('CleanCode');
   });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CleanCode app is running!');
+  });
 });
